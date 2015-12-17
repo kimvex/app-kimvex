3b680213ebd2f252f5425a213ee4e4578f85ccd8
Benjamin <chomi.numb.94@gmail.com>
2015-12-17 05:04:29 -0600

    Hacemos algunos cambios

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
 
 	function conectar(e){
-		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.1.114:5000" type="text/html"></object>';
+		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.1.133:5000" type="text/html"></object>';
 	}
 	conectar();
 });
